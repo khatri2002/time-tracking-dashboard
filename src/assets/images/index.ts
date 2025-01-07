f30cdc00d82f5843ddf7cbfdd650bcc77feaec32
Jay Khatri <jay.khatri@oorjan.com>
2025-01-07 13:12:06 +0530

    feat(react, typescript, tailwind configured)

---
+++ src/assets/images/index.ts
@@ -0,0 +1,19 @@
+import iconEllipsis from "./icon-ellipsis.svg";
+import iconExercise from "./icon-exercise.svg";
+import iconPlay from "./icon-play.svg";
+import iconSelfCare from "./icon-self-care.svg";
+import iconSocial from "./icon-social.svg";
+import iconStudy from "./icon-study.svg";
+import iconWork from "./icon-work.svg";
+import imageJeremy from "./image-jeremy.png";
+
+export {
+  iconEllipsis,
+  iconExercise,
+  iconPlay,
+  iconSelfCare,
+  iconSocial,
+  iconStudy,
+  iconWork,
+  imageJeremy,
+};
